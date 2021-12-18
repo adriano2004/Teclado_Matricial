aa80dd5b5709df5733ceacbfaa88675e7d3ecab2
Adriano dos Santos Lisboa <robertopereira23447@gmail.com>
2021-12-18 11:30:18 -0300

    MostrandoTecla
    Mostrando qual tecla foi pressionada

--- TecladoMatricial.ino
+++ TecladoMatricial.ino
@@ -1,69 +1,27 @@
-void setup()
-{
-  //Pinos ligados aos pinos 1, 2, 3 e 4 do teclado - Linhas
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-   
-  //Pinos ligados aos pinos 5, 6, 7 e 8 do teclado - Colunas
-  pinMode(8, INPUT);
-  pinMode(9, INPUT);
-  pinMode(10, INPUT);
-  pinMode(11, INPUT);
-   
-  Serial.begin(9600);
-  Serial.println("Aguardando acionamento das teclas...");
-  Serial.println();
+#include <Keypad.h>
+
+const byte linhas = 4;
+const byte colunas = 4;
+
+const char Teclas_Matriz[linhas][colunas] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'},
+};
+
+const byte Pinos_Linhas[linhas] = {9,8,7,6};
+const byte Pinos_Colunas[colunas] = {5,4,3,2};
+
+Keypad teclado_personalizado = Keypad(makeKeymap(Teclas_Matriz), Pinos_Linhas, Pinos_Colunas, linhas, colunas);
+void setup (){
+  Serial.begin (9600);
 }
- 
-void loop()
-{
-    for (int ti = 3; ti<7; ti++)
-    {
-    //Alterna o estado dos pinos das linhas
-    digitalWrite(3, LOW);
-    digitalWrite(4, LOW);
-    digitalWrite(5, LOW);
-    digitalWrite(6, LOW);
-    digitalWrite(ti, HIGH);
-    //Verifica se alguma tecla da coluna 1 foi pressionada
-    if (digitalRead(8) == HIGH)
-    {
-      imprime_linha_coluna(ti-2, 1);
-      while(digitalRead(8) == HIGH){}
+
+void loop(){
+  char leitura_teclas = teclado_personalizado.getKey();
+
+    if(leitura_teclas){
+      Serial.println(leitura_teclas);
     }
- 
-    //Verifica se alguma tecla da coluna 2 foi pressionada    
-    if (digitalRead(9) == HIGH)
-    {
-      imprime_linha_coluna(ti-2, 2);
-      while(digitalRead(9) == HIGH){};
-    }
-     
-    //Verifica se alguma tecla da coluna 3 foi pressionada
-    if (digitalRead(10) == HIGH)
-    {
-      imprime_linha_coluna(ti-2, 3);
-      while(digitalRead(10) == HIGH){}
-    }
-     
-    //Verifica se alguma tecla da coluna 4 foi pressionada
-    if (digitalRead(11) == HIGH)
-    {
-      imprime_linha_coluna(ti-2, 4);
-      while(digitalRead(11) == HIGH){} 
-    }
-   }
-   delay(10);
-}
- 
-void imprime_linha_coluna(int x, int y)
-{
-       Serial.print("Linha : ");
-       Serial.print(x);
-       Serial.print(" x Coluna : ");
-       Serial.print(y);
-       delay(10);
-       Serial.println();
 }
